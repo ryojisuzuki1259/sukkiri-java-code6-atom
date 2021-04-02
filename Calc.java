148580189f69e5df4d7f3373e7de69d2f5e19c40
鈴木良治 <suzukiryoji@suzukiyoshiharunoMacBook-Air.local>
2021-04-03 05:45:41 +0900

    Add calcapp. to Calc.java

--- Calc.java
+++ Calc.java
@@ -2,8 +2,8 @@ package calcapp.main;
 public class Calc {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
-		int total = CalcLogic.tasu(a, b);
-		int delta = CalcLogic.hiku(a, b);
+		int total = calcapp.CalcLogic.tasu(a, b);
+		int delta = calcapp.CalcLogic.hiku(a, b);
 		System.out.println("足すと" + total + "、引くと" + delta);
 	}
 
